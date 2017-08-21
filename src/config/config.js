e997946cbc0566aa71f13fba7d1096039aee9c24
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-08-21 17:09:11 -0500

    React-Firebase files

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyABGsDJQzfbiAMfqn_hfxa_QhIFkue03ns",
+    authDomain: "team-project-6be21.firebaseapp.com",
+    databaseURL: "https://team-project-6be21.firebaseio.com",
+    projectId: "team-project-6be21",
+    storageBucket: "team-project-6be21.appspot.com",
+    messagingSenderId: "962673456458"
+  };
+  
